e2f5fc2cde80189ff9ec341fabf29a49361220e5
lab <lab@wm320-a229569.campus.ithaca.lan>
2015-03-29 18:48:10 -0400

    initial design commit

---
+++ OnSet/FavoritesViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FavoritesViewController.swift
+//  OnSet
+//
+//  Created by Charles Woodward on 3/22/15.
+//  Copyright (c) 2015 Charles Woodward. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
